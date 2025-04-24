e7c536e029694471a129ee2ec19cbe404fe34a69
asseternity <nakupovasset@gmail.com>
2025-04-24 18:46:05 +0400

    feat: progress the battle system, add HP bars to unit prefabs

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Articy.Articy_Tutorial;
+using Articy.Unity;
+using Articy.Unity.Interfaces;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
@@ -11,22 +16,36 @@ public class BattleManager : MonoBehaviour
     public List<BattleParticipant> friendlies;
     public GameObject player3D;
     public GameObject battleUI;
+    public StoryManager storyManager;
+    public string[] postBattleQueue;
+    public BattleData currentBattleData;
+    private bool buttonsActive = false;
 
     public void FindBattleByID(string battleID, string[] queue)
     {
+        // clear data
+        allParticipants.Clear();
+        enemies.Clear();
+        currentBattleData = null;
+
+        // replace the queue
+        postBattleQueue = queue;
+
         foreach (BattleData battle in allBattles)
         {
             if (battle.battleID == battleID)
             {
+                // save battle data for later use
+                currentBattleData = battle;
                 // Start the battle with the found battle data
-                StartBattle(battle, queue);
+                StartBattle(battle);
                 return;
             }
         }
         Debug.LogError("Battle with ID " + battleID + " not found.");
     }
 
-    public void StartBattle(BattleData battleData, string[] queue)
+    public void StartBattle(BattleData battleData)
     {
         allParticipants = battleData.participants;
         enemies = battleData.participants.FindAll(p => !p.isPlayer && !p.isFriendly);
@@ -104,20 +123,153 @@ public class BattleManager : MonoBehaviour
         }
         // create battle UI
         battleUI.SetActive(true); // Activate the battle UI
-
-        // start battle loop: player turn, enemy turn, allies turn if any
-        // HOW TO DO:
-
-        // check if units are dead and remove them if so
-        // HOW TO DO:
-
-        // check if battle is over and revert to the story manager with the rest of the queue
-        // HOW TO DO:
     }
 
-    public void PlayerTurn()
+    public void battleStatusUpdater()
     {
-        // player turn logic here
+        // check if enemies are dead and remove them if so
+        foreach (BattleParticipant fighter in enemies)
+        {
+            if (fighter.HP <= 0)
+            {
+                allParticipants.Remove(fighter);
+                Destroy(GameObject.Find(fighter.participantName)); // Destroy the game object
+            }
+        }
+
+        // check if player is dead and end the game if so
+        if (player.HP <= 0)
+        {
+            Debug.Log("Game Over! Player is dead.");
+            return;
+        }
+
+        // check if battle is over and revert to the story manager with the rest of the queue
+        if (enemies.Count == 0)
+        {
+            string nextThing = postBattleQueue[0];
+            string nextThingType = nextThing.Split('_')[0];
+            string nextThingID = nextThing.Split('_')[1];
+
+            // clear the battle UI
+            battleUI.SetActive(false); // Deactivate the battle UI
+
+            // teleport back to the original location
+            PlayerController playerController = player3D.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.isInBattle = false; // Set the battle flag to false
+            }
+            else
+            {
+                Debug.LogError("PlayerController not found on player object.");
+            }
+
+            // destroy the battle environment
+            string battleEnvironmentName = currentBattleData.battleEnvironmentPrefab.name;
+            Destroy(GameObject.Find(battleEnvironmentName)); // Destroy the battle environment
+
+            // destroy the player
+            Destroy(GameObject.Find(player.participantName)); // Destroy the player object
+
+            // play the next thing in the queue
+            if (nextThingType == "dialogue")
+            {
+                var nextDialogue = ArticyDatabase.GetObject(nextThingID) as DialogueFragment;
+                if (nextDialogue != null)
+                {
+                    // cut the queue to remove the first element
+                    string[] nextThingQueue = new string[postBattleQueue.Length - 1];
+                    Array.Copy(postBattleQueue, 1, nextThingQueue, 0, postBattleQueue.Length - 1);
+                    // start the next dialogue
+                    storyManager.StartSectionFromDialogue(nextDialogue, nextThingQueue);
+                }
+                else
+                {
+                    Debug.LogError("Next dialogue not found: " + nextThingID);
+                }
+            }
+            else if (nextThingType == "battle")
+            {
+                string[] nextThingQueue = new string[postBattleQueue.Length - 1];
+                Array.Copy(postBattleQueue, 1, nextThingQueue, 0, postBattleQueue.Length - 1);
+                storyManager.StartSectionFromBattle(nextThingID, nextThingQueue);
+            }
+            else if (nextThingType == "nothing")
+            {
+                Debug.LogError("No more things in the queue after battle.");
+            }
+        }
+    }
+
+    public void ToggleButtons()
+    {
+        // 1. unblock the ui buttons
+        GameObject[] buttons = new GameObject[6];
+        buttons[0] = GameObject.Find("AttackButton");
+        buttons[1] = GameObject.Find("SpellButton");
+        buttons[2] = GameObject.Find("ItemButton");
+        buttons[3] = GameObject.Find("SkipButton");
+        buttons[4] = GameObject.Find("CharacterButton");
+        buttons[5] = GameObject.Find("MenuButton");
+
+        foreach (GameObject button in buttons)
+        {
+            if (button != null)
+            {
+                if (buttonsActive)
+                {
+                    button.GetComponent<Button>().interactable = false; // Enable the button
+                    buttonsActive = false; // Set the flag to false
+                }
+                else
+                {
+                    button.GetComponent<Button>().interactable = true; // Disable the button
+                    buttonsActive = true; // Set the flag to true
+                }
+            }
+            else
+            {
+                Debug.LogError("Button not found: " + button.name);
+            }
+        }
+    }
+
+    // make functions:
+    // 2. get player's input for this turn's action
+    // 3. if the action is an attack, change the cursor to a crosshair and allow the player to select a target
+    // 4. then attack the target and deal damage
+    // 5. end the turn automatically, disabling the ui buttons
+    // 6. call battleStatusUpdater() to check if the battle is over
+    // 7. call the enemies turn function
+
+    public void OnActionButtonClicked(string actionType)
+    {
+        switch (actionType)
+        {
+            case "Attack":
+                // Change cursor to crosshair and allow player to select a target
+                // Implement target selection logic here
+                break;
+            case "Spell":
+                // Implement spell casting logic here
+                break;
+            case "Item":
+                // Implement item usage logic here
+                break;
+            case "Skip":
+                // Skip the turn logic here
+                break;
+            case "Character":
+                // Show character stats or abilities here
+                break;
+            case "Menu":
+                // Open the menu here
+                break;
+            default:
+                Debug.LogError("Unknown action type: " + actionType);
+                break;
+        }
     }
 
     public void EnemiesTurn()
--- Assets/To Do.cs
+++ Assets/To Do.cs
@@ -14,19 +14,30 @@
 // [v] learn articy: add sprites to dialogues
 // [v] learn articy: presentation view
 // [v] articy naming - and using vars in dialogues, like for pronouns
-// [_] dialogue UI prettying - layers, shadows, make models appear
+// [v] some kind of story manager that determines which things are available and which indicators to show
+// [v] some kind of quests system that envelops everything, including the articy things
+// [_] (2) dialogue UI prettying - layers, shadows, make models appear
+// [_] (3) calendar system
+// [_] (3) activities as per below
+// [_] (3) quests as per below
+// [_] saving / loading
+// [_] main menu
+// [_] pause menu
+// [_] settings menu
+// [_] game over
 // [_] some visual indicator if there's a dialogue available
-// [_] calendar system
-// [_] some kind of story manager that determines which things are available and which indicators to show
-// [_] some kind of quests system that envelops everything, including the articy things
 
 // persona fighting mechanics:
 // [v] build the story manager script with 2 functions: startDialogue (articyObject, nextThing) and startBattle (battleID, nextThing)
 // [v] create a scriptable object for battle participant
 // [v] create a scriptable object for battle data that will include participants and battleID for lookup
 // [v] create a function that will take battleID as an argument to be called from story manager to start the battle
-// [_] meat of the battle --- this function will take away movement controls, change camera, and start the battle mechanics
-// [_] get transported to "fighting realm scene", spawn enemies, then mechanics activate
+// [v] get transported to "fighting realm scene", spawn enemies, then mechanics activate
+// [v] meat of the battle --- this function will take away movement controls, change camera, and start the battle mechanics
+// [v] show health bars in prefabs for all participants
+// [_] (0) make enemies clickable and outline the currently clicked enemy
+// [_] (1) working battle loop
+// [_] using stats and abilities for all participants and where to store them
 // [_] camera angles, particle effects
 // [_] build the quest and activity scriptable objects
 
